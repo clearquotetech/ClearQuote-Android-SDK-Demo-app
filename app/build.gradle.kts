5040e4491157aa477aa0d5df8a1808a4dd4c6043
sanketbhangale43 <handsome9987386381>
2023-11-28 19:22:59 +0530

    Updated app to a latest dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,5 +77,5 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
 
     // Maven local
-    implementation("io.clearquote.assessment.cq_sdk:cq-android-sdk:2.0.1@aar") { isTransitive = true }
+    implementation ("com.github.clearquotetech:cq-android-sdk:2.0.1-test")
 }
