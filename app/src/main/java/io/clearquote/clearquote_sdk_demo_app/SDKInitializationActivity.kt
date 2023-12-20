81d0fb453c692af6e6eb321e794ad1afc97e4bd2
sanketbhangale43 <handsome9987386381>
2023-12-20 19:39:03 +0530

    Removed user name and location inputs from the sdk initialization activity

--- app/src/main/java/io/clearquote/clearquote_sdk_demo_app/SDKInitializationActivity.kt
+++ app/src/main/java/io/clearquote/clearquote_sdk_demo_app/SDKInitializationActivity.kt
@@ -2,10 +2,7 @@ package io.clearquote.clearquote_sdk_demo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
 import io.clearquote.assessment.cq_sdk.CQSDKInitializer
-import io.clearquote.assessment.cq_sdk.R
-import io.clearquote.assessment.cq_sdk.datasources.remote.network.datamodels.createQuoteApi.payload.CreatedByAttrs
 import io.clearquote.assessment.cq_sdk.singletons.PublicConstants
 import io.clearquote.clearquote_sdk_demo_app.databinding.ActivitySdkInitializationBinding
 import io.clearquote.clearquote_sdk_demo_app.support.ErrorDialog
@@ -32,10 +29,6 @@ class SdkInitializationActivity : AppCompatActivity() {
 
         // Initialize vars
         cqSDKInitializer = CQSDKInitializer(this)
-        val sharedPreferences = getSharedPreferences(
-            app_shared_preferences_file_name,
-            MODE_PRIVATE
-        )
         sdkInitializationDialog = LoadingDialog(this, "Initializing ClearQuote SDK")
 
         // Set click listener on the save button
@@ -55,30 +48,11 @@ class SdkInitializationActivity : AppCompatActivity() {
 
                     // Check response
                     if (isInitialized && code == PublicConstants.sdkInitializationSuccessCode) {
-
                         // Save key in the shared preferences
-                        sharedPreferences.edit().putString(cq_sdk_key, enteredSdkKey).apply()
+                        getSharedPreferences(app_shared_preferences_file_name, MODE_PRIVATE).edit().putString(cq_sdk_key, enteredSdkKey).apply()
 
-                        // Start inspection
-                        cqSDKInitializer.startInspection(
-                            activityContext = this,
-                            createdByAttrs = CreatedByAttrs(
-                                userName = binding.etUserName.text.toString().trim(),
-                                location = binding.etLocation.text.toString().trim()
-                            ),
-                            result = { isStarted, msg ->
-                                // Dismiss loading dialog
-                                sdkInitializationDialog?.dismiss()
-
-                                // finish the activity
-                                finish()
-
-                                // Show error if required
-                                if (!isStarted) {
-                                    showErrorDialog(message = msg)
-                                }
-                            }
-                        )
+                        // Finish activity
+                        finish()
                     } else {
                         showErrorDialog(message = message)
                     }
