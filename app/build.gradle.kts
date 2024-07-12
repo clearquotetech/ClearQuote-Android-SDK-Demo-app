c63eaf784fa94d50430a6ff7989ceddec4ebba11
sanketbhangale43 <handsome9987386381>
2024-07-12 18:20:14 +0530

    updates in versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
 
     // Maven local
-    implementation("io.clearquote.assessment.cq_sdk:cq-android-sdk:2.2.2@aar") { isTransitive = true }
+    implementation("io.clearquote.assessment.cq_sdk:cq-android-sdk:2.2.3@aar") { isTransitive = true }
 
     // Leak canary
     debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.14")
