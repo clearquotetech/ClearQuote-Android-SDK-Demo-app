511030c481dfac2bf400946d06c5485069cc31ae
Sanket Bhangale <66669902+sanketbhangale43@users.noreply.github.com>
2025-07-21 12:27:18 +0530

    Incremented sdk version to 2.6.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
 
     // Maven local
-    implementation("io.clearquote.assessment.cq_sdk:cq-android-sdk:2.6.2@aar") { isTransitive = true }
+    implementation("io.clearquote.assessment.cq_sdk:cq-android-sdk:2.6.2.1@aar") { isTransitive = true }
 
     // Leak canary
     // debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.14")
