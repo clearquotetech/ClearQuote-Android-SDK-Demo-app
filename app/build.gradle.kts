c69fb2c39e829ddf7d9f3b0335a6ae105d3f4bd1
ArjunJadeja-CQ <arjunjadeja@clearquote.io>
2026-07-30 10:03:06 +0530

    Updated Demo App with latest SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 30
-        versionName = "4.6"
+        versionName = "4.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
