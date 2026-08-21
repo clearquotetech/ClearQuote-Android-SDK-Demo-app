1ba5e27cd94dfa40ef669631b9f3bdebb450f7d3
Udit Bhaskar <udit@clearquote.io>
2026-08-21 10:39:44 +0530

    Adjust Repair Estimate list layout weights and update sample cost

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.clearquote.clearquote_sdk_demo_app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "4.9"
+        versionCode = 31
+        versionName = "4.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
